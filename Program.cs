cfc4c15c2875c1a8bcdb2993f00c0d98241ed7a0
rxgq <ashtondunderdale@gmail.com>
2025-01-17 19:08:44 +0000

    enchilada racing

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿internal class Program {
+    static void Main() {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- src/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿internal abstract class Program {
-    static void Main() {
-        var source = File.ReadAllText("example/mermaid.txt");
-
-        var transpiler = new Transpiler();
-        transpiler.Generate(source);
-    }
-}
